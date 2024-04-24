75f4af98617e8b618a441b55fbba01c4622a8a49
TallesPacheco <tallesd2@hotmail.com>
2024-04-24 08:29:30 -0300

    começando a fazer o front, header já finalizando, marcado o local do body e começo do footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 
 export function App() {
   return (
-    <h1>Hello</h1>
+    <div className="min-h-screen flex flex-col">
+    	<div className="px-6 py-3 flex items-center justify-between border-b">
+        	<h1 className="text-xl font-bold ">Upload.ai</h1>
+
+        	<div className="flex items-center gap-3">
+          		<span className="text-sm text-muted-foreground">Desenvolvido com ❤️</span>
+        	</div>
+      	</div>
+
+		<main className="flex-1 p-6 flex gap-6">
+			<div className="flex flex-col flex-1 gap-4">
+				<div className="grid grid-rows-2 gap-4 flex-1"></div>
+
+
+				<p className="text-sm text-muted-foreground">Lembre-se: você pode utilizar a variável transcription no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
+			</div>
+
+			<aside className="w-80"></aside>
+		</main>
+
+    </div>
   )
-}
+} 
